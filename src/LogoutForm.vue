10b005b2d0b6c21e4f1a626615ebb8a32651c28f
student15 <student15@pcoip.ki.agh.edu.pl>
2019-04-27 15:48:30 +0200

    LoginForm i LogoutForm finnishedgit status and dodawanie spotkania

--- src/LogoutForm.vue
+++ src/LogoutForm.vue
@@ -1,12 +1,32 @@
 <template>
    <div>
-       <label>Wyloguj sie {{ username }}</label>
-       <button @click="$emit('logout')">Wyloguj</button>
+      	 <label>Wyloguj sie {{ username }}</label>
+      	 <button @click="$emit('logout')">Wyloguj</button>
+      	 <h2>Spotkania</h2>
+      	 <button @click="addNewMeeting()">Dodaj spotknie</button>
+      	 <label>{{ meetings }}</label>
+      	 <ol>
+ 	   		<li v-for="item in meetings" :key="item.id">{{ item }}</li>
+		</ol>
    </div>
 </template>
 
 <script>
 export default {
 	props: ['username'],
+	data(){
+		return {
+			meetings: [
+				'MOW',
+				'TO',
+				'MIS'
+			]
+		};
+	},
+	methods: {
+    	addNewMeeting() {
+       		this.meetings.push('nowe');
+   	 	}
+}
 }
 </script>
